6d58374ba541dca021222b08847d6936cc714912
忍者パンダ <399812+ninjapanda@users.noreply.github.com>
2023-05-17 20:56:01 +0800

    Update

--- js/index.js
+++ js/index.js
@@ -9,13 +9,21 @@ $(window).on("scroll", function () {
     if (el.hasClass("header-background")) {
       $("#scroll-fix1").removeClass("indexHover");
       $("#scroll-fix2").removeClass("indexHover");
+      $("#scroll-fix3").removeClass("indexHover");
+      $("#scroll-fix4").removeClass("indexHover");
       $("#scroll-fix1").addClass("scrollDownNavbarStyle");
       $("#scroll-fix2").addClass("scrollDownNavbarStyle");
+      $("#scroll-fix3").addClass("scrollDownNavbarStyle");
+      $("#scroll-fix4").addClass("scrollDownNavbarStyle");
     }
   } else {
     $("#scroll-fix1").addClass("indexHover");
     $("#scroll-fix2").addClass("indexHover");
+    $("#scroll-fix3").addClass("indexHover");
+    $("#scroll-fix4").addClass("indexHover");
     $("#scroll-fix1").removeClass("scrollDownNavbarStyle");
     $("#scroll-fix2").removeClass("scrollDownNavbarStyle");
+    $("#scroll-fix3").removeClass("scrollDownNavbarStyle");
+    $("#scroll-fix4").removeClass("scrollDownNavbarStyle");
   }
 });
